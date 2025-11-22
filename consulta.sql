5f7c820c97ab55439a4d70ee6c74f07971274017
Guilherme Sousa <108591854+Guisousa5@users.noreply.github.com>
2025-11-22 01:27:54 -0300

    Refactor SQL queries with comments for clarity

--- consulta.sql
+++ consulta.sql
@@ -1,15 +1,45 @@
-Select Nome,Ano from dbo.Filmes;
-Select Nome,Ano,Duracao from dbo.Filmes order by Ano ASC ;
+-- 1. Buscar o nome e ano dos filmes
+SELECT Nome, Ano FROM dbo.Filmes;
 
-Select Nome,Ano,Duracao from dbo.Filmes where Nome = 'De Volta para o Futuro';
+-- 2. Buscar o nome, ano e duração dos filmes, ordenados pelo ano (crescente)
+SELECT Nome, Ano, Duracao FROM dbo.Filmes ORDER BY Ano ASC;
 
-Select * from dbo.Filmes where Ano = 1997;
-Select * from dbo.Filmes where Ano > 2000;
-Select Nome,Ano,Duracao from dbo.Filmes where Duracao BETWEEN 101 AND 149 ;
-select ano,count(nome) as Quantidade from dbo.Filmes group by ano order by Quantidade desc;
-select * from Atores where genero = 'M';
-select * from Atores where genero = 'F' order by PrimeiroNome;
+-- 3. Buscar pelo filme "De Volta para o Futuro", trazendo nome, ano e duração
+SELECT Nome, Ano, Duracao FROM dbo.Filmes WHERE Nome = 'De Volta para o Futuro';
 
-Select a.Nome,g.Genero from dbo.Filmes as a  join FilmesGenero as b on b.IdFilme = a.Id join Generos as g on g.Id = b.IdGenero;
-Select a.Nome,g.Genero from dbo.Filmes as a  join FilmesGenero as b on b.IdFilme = a.Id join Generos as g on g.Id = b.IdGenero where Genero = 'Mistério';
-Select a.Nome, ato.PrimeiroNome, ato.UltimoNome, ef.Papel from dbo.Filmes AS a join ElencoFilme  as ef on ef.IdFilme = a.Id join Atores as ato on ato.Id = ef.IdAtor;
+-- 4. Buscar os filmes lançados em 1997
+SELECT * FROM dbo.Filmes WHERE Ano = 1997;
+
+-- 5. Buscar os filmes lançados após o ano 2000
+SELECT * FROM dbo.Filmes WHERE Ano > 2000;
+
+-- 6. Buscar filmes com duração entre 101 e 149 minutos
+SELECT Nome, Ano, Duracao FROM dbo.Filmes WHERE Duracao BETWEEN 101 AND 149 ORDER BY Duracao ASC;
+
+-- 7. Buscar a quantidade de filmes lançados por ano, ordenando pela quantidade (decrescente)
+SELECT Ano, COUNT(Nome) AS Quantidade FROM dbo.Filmes GROUP BY Ano ORDER BY Quantidade DESC;
+
+-- 8. Buscar os atores do gênero masculino
+SELECT * FROM dbo.Atores WHERE Genero = 'M';
+
+-- 9. Buscar os atores do gênero feminino, ordenando pelo PrimeiroNome
+SELECT * FROM dbo.Atores WHERE Genero = 'F' ORDER BY PrimeiroNome;
+
+-- 10. Buscar o nome do filme e o gênero
+SELECT f.Nome, g.Genero
+FROM dbo.Filmes f
+JOIN dbo.FilmesGenero fg ON fg.IdFilme = f.Id
+JOIN dbo.Generos g ON g.Id = fg.IdGenero;
+
+-- 11. Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT f.Nome, g.Genero
+FROM dbo.Filmes f
+JOIN dbo.FilmesGenero fg ON fg.IdFilme = f.Id
+JOIN dbo.Generos g ON g.Id = fg.IdGenero
+WHERE g.Genero = 'Mistério';
+
+-- 12. Buscar o nome do filme, atores e seus papéis
+SELECT f.Nome, a.PrimeiroNome, a.UltimoNome, ef.Papel
+FROM dbo.Filmes f
+JOIN dbo.ElencoFilme ef ON ef.IdFilme = f.Id
+JOIN dbo.Atores a ON a.Id = ef.IdAtor;
